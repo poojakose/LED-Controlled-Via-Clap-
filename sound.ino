66ac2d9f38bbc19199952e9227be5e89abf018ea
Pooja Kose <30698792+poojakose@users.noreply.github.com>
2020-04-25 15:43:28 +0530

    Add files via upload

---
+++ sound.ino
@@ -0,0 +1,22 @@
+int s = A0;
+int l = 7;
+void setup()
+{
+  pinMode(l, OUTPUT);
+  Serial.begin(9600);
+}
+
+void loop()
+{
+  int s=analogRead (s);
+  Serial.println(s);
+  delay(200);
+  if (s > 552)
+  {
+    digitalWrite(l,HIGH);
+  }
+  else
+  {
+  digitalWrite(l,LOW);
+  }
+}
